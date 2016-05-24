4ca714ccffd32bbefccb7d240e9eb9499b1c291d
Francisco Mazzoni <fran.mazzoni@gmail.com>
2016-05-24 09:58:41 -0300

    added map functionalities

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
-require(['bower_components/knockout/dist/knockout.js', '../viewModels/appViewModel', 'bower_components/domready/ready.min.js'], function(ko, appViewModel) {
-    ko.applyBindings(new appViewModel());
+function map(lat = 41.8339042, long = -88.0123461) {
+  this.mapOptions = {
+    zoom: 10,
+    center: new google.maps.LatLng(lat,long),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.map = new google.maps.Map(document.getElementById('map'), this.mapOptions);
+};
+
+function appViewModel() {
+  var self = this;
+
+  self.firstName = ko.observable('Bert');
+  self.lastName = ko.observable('Rodriguez');
+  self.firstNameCaps = ko.pureComputed(function() {
+      return self.firstName().toUpperCase();
+  });
+
+  self.mapMarkers = ko.observableArray([]); //List of markers for the mapMarkers
+  self.neighborHood = ko.observable('Chicago'); //Selected neighborhood
+  self.latitude = ko.observable(41.8339042); //Selected neighborhood map lat
+  self.longitude = ko.observable(-88.0123461);
+
+  self.toggleList = ko.observable(); //Toggle button for hiding/showing list
+
+  self.map = map(self.latitude(), self.longitude());
+
+
+
+};
+
+$(function(){
+  ko.applyBindings(new appViewModel());
 });
